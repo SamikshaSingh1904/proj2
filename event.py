2465a1844fe205b6eaefcd87a1717d090c688b03
beatrixkim <bb.km.2358@gmail.com>
2025-12-02 17:06:12 -0500

    changed based on feedback

--- event.py
+++ event.py
@@ -17,38 +17,11 @@ def format_time(time_delta):
         display_hours = 12
     return f'{display_hours:02d}:{minutes:02d} {period}'
 
-def get_all_active_events():
-    """
-    Fetch ALL events (all statuses) for displaying all events on calendar
-    Returns list of event dictionaries with formatted times
-    """
-    conn = dbi.connect()
-    curs = dbi.dict_cursor(conn)
-    
-    query = '''
-        SELECT e.eid, e.title, e.start, e.end, e.date, e.desc,
-               e.city, e.state, e.cap, c.category
-        FROM events e
-        JOIN calendar c ON e.cid = c.cid
-        ORDER BY e.date, e.start
-    '''
-    
-    curs.execute(query)
-    events = curs.fetchall()
-    
-    # Format times for display
-    for event in events:
-        event['start_formatted'] = format_time(event['start'])
-        event['end_formatted'] = format_time(event['end'])
-    
-    return events
-
-def get_week_events(start_date, end_date):
+def get_week_events(conn, start_date, end_date):
     """
     Fetch all events for a given week
     Returns list of event dictionaries with formatted times
     """
-    conn = dbi.connect()
     curs = dbi.dict_cursor(conn)
     
     query = '''
@@ -76,12 +49,11 @@ def get_week_events(start_date, end_date):
     
     return events
 
-def get_event_by_id(eid):
+def get_event_by_id(conn, eid):
     """
     Get full details of a specific event including creator info
     Returns event dictionary or None if not found
     """
-    conn = dbi.connect()
     curs = dbi.dict_cursor(conn)
     
     query = '''
@@ -97,12 +69,11 @@ def get_event_by_id(eid):
     curs.execute(query, (eid,))
     return curs.fetchone()
 
-def get_event_participants(eid):
+def get_event_participants(conn, eid):
     """
     Get all participants for a specific event
     Returns list of participant dictionaries
     """
-    conn = dbi.connect()
     curs = dbi.dict_cursor(conn)
     
     query = '''
@@ -116,12 +87,11 @@ def get_event_participants(eid):
     curs.execute(query, (eid,))
     return curs.fetchall()
 
-def get_participant_count(eid):
+def get_participant_count(conn, eid):
     """
     Get current number of participants for an event
     Returns integer count
     """
-    conn = dbi.connect()
     curs = dbi.cursor(conn)
     
     query = 'SELECT COUNT(*) FROM participants WHERE eid = %s'
